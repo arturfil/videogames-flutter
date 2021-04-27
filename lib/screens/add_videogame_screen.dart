d1a96a99a255f7b034d484f9443fa3622dbd7669
arturfil <arturfil@hotmail.com>
2021-04-27 19:09:02 -0400

    added videogames add screen

--- lib/screens/add_videogame_screen.dart
+++ lib/screens/add_videogame_screen.dart
@@ -78,7 +78,7 @@ class _AddVideoGameScreenState extends State<AddVideoGameScreen> {
                         child: TextFormField(
                           style: TextStyle(fontSize: 18.0),
                           decoration: InputDecoration(
-                            labelText: 'Title',
+                            labelText: 'Name',
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10.0),
                             ),
@@ -90,82 +90,91 @@ class _AddVideoGameScreenState extends State<AddVideoGameScreen> {
                           initialValue: _name,
                         ),
                       ),
-                      // Padding(
-                      //   padding: EdgeInsets.symmetric(vertical: 20.0),
-                      //   child: DropdownButtonFormField(
-                      //     isDense: true,
-                      //     icon: Icon(Icons.arrow_drop_down_circle),
-                      //     iconSize: 22.0,
-                      //     iconEnabledColor: Theme.of(context).primaryColor,
-                      //     items: _priorities.map((String priority) {
-                      //       return DropdownMenuItem(
-                      //         value: priority,
-                      //         child: Text(
-                      //           priority,
-                      //           style: TextStyle(
-                      //             color: Colors.black,
-                      //             fontSize: 18.0,
-                      //           ),
-                      //         ),
-                      //       );
-                      //     }).toList(),
-                      //     style: TextStyle(fontSize: 18.0),
-                      //     decoration: InputDecoration(
-                      //       labelText: 'Priority',
-                      //       labelStyle: TextStyle(fontSize: 18.0),
-                      //       border: OutlineInputBorder(
-                      //         borderRadius: BorderRadius.circular(10.0),
-                      //       ),
-                      //     ),
-                      //     validator: (input) => _priority == null
-                      //         ? 'Please select a priority level'
-                      //         : null,
-                      //     onSaved: (input) => _priority = input,
-                      //     onChanged: (value) {
-                      //       setState(() {
-                      //         _priority = value;
-                      //       });
-                      //     },
-                      //     value: _priority),
-                      // ),
-                      // Container(
-                      //   margin: EdgeInsets.symmetric(vertical: 20.0),
-                      //   height: 60.0,
-                      //   width: double.infinity,
-                      //   decoration: BoxDecoration(
-                      //     color: Theme.of(context).primaryColor,
-                      //     borderRadius: BorderRadius.circular(30.0),
-                      //   ),
-                      //   child: FlatButton(
-                      //     child: Text(
-                      //       widget.task == null ? 'Add' : 'Update',
-                      //       style: TextStyle(
-                      //         color: Colors.white,
-                      //         fontSize: 20.0,
-                      //       ),
-                      //     ),
-                      //     onPressed: _submit,
-                      //   ),
-                      // ),
-                    //   widget.task != null ? Container(
-                    //     margin: EdgeInsets.symmetric(vertical: 20.0),
-                    //     height: 60.0,
-                    //     width: double.infinity,
-                    //     decoration: BoxDecoration(
-                    //       color: Theme.of(context).primaryColor,
-                    //       borderRadius: BorderRadius.circular(30.0),
-                    //     ),
-                    //     child: FlatButton(
-                    //       child: Text(
-                    //         'Delete',
-                    //         style: TextStyle(
-                    //           color: Colors.white,
-                    //           fontSize: 20.0,
-                    //         ),
-                    //       ),
-                    //       onPressed: _delete,
-                    //     ),
-                    //   ) :  SizedBox.shrink(),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Price',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input.trim().isEmpty
+                              ? 'Please enter a task title'
+                              : null,
+                          onSaved: (input) => _name = input,
+                          initialValue: _name,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Category',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input.trim().isEmpty
+                              ? 'Please enter a task title'
+                              : null,
+                          onSaved: (input) => _name = input,
+                          initialValue: _name,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Description',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input.trim().isEmpty
+                              ? 'Please enter a task title'
+                              : null,
+                          onSaved: (input) => _name = input,
+                          initialValue: _name,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Quantity',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input.trim().isEmpty
+                              ? 'Please enter a task title'
+                              : null,
+                          onSaved: (input) => _name = input,
+                          initialValue: _name,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Photo',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input.trim().isEmpty
+                              ? 'Please enter a task title'
+                              : null,
+                          onSaved: (input) => _name = input,
+                          initialValue: _name,
+                        ),
+                      ),
                     ],
                   ),
                 )
